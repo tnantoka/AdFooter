04acb12f98cd9ddb146ddc97b7ee14018cf273d4
tnantoka <tnantoka@bornneet.com>
2016-01-24 18:57:53 +0900

    Add hidden flag

--- Pod/Classes/AdFooter.swift
+++ Pod/Classes/AdFooter.swift
@@ -12,6 +12,13 @@ public class AdFooter {
     public static let shared = AdFooter()
     
     public var adMobAdUnitId = ""
+    public var hidden = false {
+        didSet {
+            controllers.forEach { $0.value?.hidden = hidden }
+        }
+    }
+    
+    var controllers = [Weak<AdFooterViewController>]()
     
     public func wrap(originalController: UIViewController) -> UIViewController {
         return wrap(originalController, withIAd: true)
@@ -19,6 +26,7 @@ public class AdFooter {
     
     public func wrap(originalController: UIViewController, withIAd: Bool) -> UIViewController {
         let adFooterController = AdFooterViewController(originalController: originalController, withIAd: withIAd)
+        controllers.append(Weak(value: adFooterController))
         return adFooterController
     }
 }
---
+++ Pod/Classes/Weak.swift
@@ -0,0 +1,16 @@
+//
+//  Weak.swift
+//  Pods
+//
+//  Created by Tatsuya Tobioka on 1/24/16.
+//
+//
+
+import Foundation
+
+class Weak<T: AnyObject> {
+    weak var value : T?
+    init(value: T) {
+        self.value = value
+    }
+}
