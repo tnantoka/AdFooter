63640dc8373ed663598309e75e294eb8f42756ed
tnantoka <tnantoka@bornneet.com>
2018-07-16 23:39:56 +0900

    Add Example/Pods for Carthage

---
+++ Example/Pods/Target Support Files/AdFooter/AdFooter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AdFooter : NSObject
+@end
+@implementation PodsDummy_AdFooter
+@end
---
+++ Example/Pods/Target Support Files/AdFooter/AdFooter-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double AdFooterVersionNumber;
+FOUNDATION_EXPORT const unsigned char AdFooterVersionString[];
+
